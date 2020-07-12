f038d04adfb28c66aa5f0755c67ff06bec8187ec
unknown <chrisnut12@gmail.com>
2020-07-12 19:41:15 +0000

    a

--- Revsions/Arrays and Related Func Programming/ranes.cpp
+++ Revsions/Arrays and Related Func Programming/ranes.cpp
@@ -1,12 +1,5 @@
-#include <range/v3/all.hpp>
 #include <iostream>
-#include <array>
-#include <numeric>
-#include <ctime>
-#include <random>
-#include <iomanip>
-using namespace ranges;
-int main(){
-    auto values = views::iota(1, 11);
+#include <all.hpp>
 
+    std::cout << "Hello World";
 }
