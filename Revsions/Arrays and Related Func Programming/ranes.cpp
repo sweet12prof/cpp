29fd752b36fdd4a077cf05ee08f985a25eb248cb
unknown <chrisnut12@gmail.com>
2020-07-18 16:00:32 +0000

    Little bits with ranges c++20

--- Revsions/Arrays and Related Func Programming/ranes.cpp
+++ Revsions/Arrays and Related Func Programming/ranes.cpp
@@ -1,5 +1,43 @@
 #include <iostream>
-#include <all.hpp>
+#include <algorithm>
+#include <numeric>
+#include <range/v3/all.hpp>
+#include <string>
+#include <array>
+#include <vector>
+using namespace ranges;
+std::array<int, 5> someFunc(std::array<int, 5>);
 
-    std::cout << "Hello World";
+int main(){
+    auto showValues = [](auto & someContainer) {
+        for (auto  item : someContainer) {
+            std::cout << item << " ";
+        }
+    };
+
+    auto createArray = views::iota(1, 11);
+    auto val2 = createArray | views::filter([](const auto& a) {return a % 2 == 0; });
+    auto val3 = val2 | views::transform([](const auto& a) {return "abe"; });
+    
+    showValues(createArray);
+    
+    std::cout << std::endl << std::endl;
+    showValues(val2);
+    std::cout << std::endl << std::endl;
+    showValues(val3);
+    std::cout << std::endl << std::endl;
+    auto val4 = std::accumulate(val2.begin(), val2.end(), 0, [](auto x, auto y) {return x - y; });
+    std::cout << val4;
+
+    std::array <int, 5> m = {1,32,1, -1};
+    for (auto item : m)
+        std::cout << item <<" ";
+    std::cout << std::endl << std::endl;
+    std::vector <int> newVector = { 1,2,3,45 };
+    auto newVal = newVector | views::filter([](const int& x) {
+        return x % 2 == 1;
+    });
+
+    showValues(newVal);
+
 }
