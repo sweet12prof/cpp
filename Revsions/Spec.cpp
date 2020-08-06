d14bd615617cfbf4dc9899ed5e45015db74a289c
unknown <chrisnut12@gmail.com>
2020-08-06 10:34:51 +0000

    Spectre proof of concept

---
+++ Revsions/Spec.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <array>
+int main(){
+    int x, y;
+    constexpr size_t array1_size =12;
+
+    std::array<int, array1_size> array2;
+    std::array<int, array1_size> array1;
+
+
+if (x < array1_size)
+    y = array2[array1[x] * 4096];
+}
