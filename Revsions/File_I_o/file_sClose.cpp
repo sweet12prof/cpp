86c9efc40191267120a798fff2e85a19cef5db7f
unknown <chrisnut12@gmail.com>
2020-08-08 06:49:10 +0000

    round_up i_o

--- Revsions/File_I_o/file_sClose.cpp
+++ Revsions/File_I_o/file_sClose.cpp
@@ -9,9 +9,12 @@ using std::cerr;
 using std::endl;
 
 int main(){
-    std::ifstream input{"clients.txt", std::ios::in};
+    std::ifstream input{"Titss.txt", std::ios::in};
     
     if( std::string inName, inNumber; input){
+        
+        input.seekg(41);
+        input.seekg(20, std::ios::end);
         while(input >> inName >> inNumber){
             cout << std::setw(20) << std::left << inName << std::setw(20) << std::left << inNumber << std::endl;
         }
